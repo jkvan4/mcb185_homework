4c13503dbc8e866f98a4e6db9f35923244306065
Julia Van <jkvan@JVAN.localdomain>
2026-01-22 14:44:37 -0800

    update

---
+++ 10demo.py
@@ -0,0 +1,19 @@
+# 10demo.py by juliaa
+
+print('hello again') # greeting
+
+import math
+
+print(1.5e-2)
+print(1 + 1)
+
+# exponent
+print(2**3)
+print(pow(2,3))
+
+# sq root
+print(2**0.5)
+print(math.sqrt(2))
+
+print(math.log(2))
+
